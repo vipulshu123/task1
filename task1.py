14edd1830310bedcbb39b4c26ed490cb539be5b4
vipulshu123 <vipul2799shukla@gmail.com>
2025-04-13 11:28:47 -0700

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,12 @@
+a=input("first number: ")
+b=input("second number: ")
+a=int(a)
+b=int(b)
+c=a+b
+print("addition is: ",c)
+d=a-b
+print("subtraction is: ",d)
+e=a*b
+print("multiplication is: ",e)
+f=a/b
+print("division is: ",f)
---
+++ task2.py
@@ -0,0 +1,3 @@
+a=input("first name: ")
+b=input("second name: ")
+print("hello, " + a + " " + b + "!" + " Welcome" + " to" + " the" + " Python" + " program")
